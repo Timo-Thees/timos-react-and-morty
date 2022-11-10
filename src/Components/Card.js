edcace072b89de4fb12eecde6fd4efc5dbbd92d7
Timo Thees <timo.thees@gmx.net>
2022-11-10 15:55:58 +0100

    I created a markup, Morty

---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+export function Card() {
+  return (
+    <div>
+      <img
+        src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        alt="Default"
+      />
+      <h2>Morty Smith</h2>
+      <button>Show more</button>
+    </div>
+  );
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Rick and Morty App</h1>
+    </header>
+  );
+}
